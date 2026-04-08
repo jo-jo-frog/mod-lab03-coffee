b4430c73342c45a883cf6aa9cd8fcd588aa81153
jo-jo-frog <suskokirill45@gmail.com>
2026-04-08 23:28:02 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,11 @@
 
+void On();
+void Off();
+void Coin(double deposit);
+void etMenu(int choice);
+void getState();
+void Choice(int choice);
+bool Check(int choice);
+void Cancel();
+void Cook(int choice);
+void Finish(int choice);
